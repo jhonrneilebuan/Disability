f63d60f64816e42dc1f5a4576287e4bc65af5779
jhonrneilebuan <160509990+jhonrneilebuan@users.noreply.github.com>
2025-03-07 23:33:57 +0800

    update

--- backend/config/cors.config.js
+++ backend/config/cors.config.js
@@ -2,4 +2,6 @@ export const corsOptions = {
   origin: ["https://disability-frontend.onrender.com"],
   methods: ["GET", "PUT", "POST", "DELETE", "PATCH"],
   credentials: true,
+  optionsSuccessStatus: 200,
+  preflightContinue: false, // Ensure the preflight response is sent
 };
