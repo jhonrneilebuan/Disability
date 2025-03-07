b78780ab13bdd34d21509789cd9ca6e0b14cf453
jhonrneilebuan <160509990+jhonrneilebuan@users.noreply.github.com>
2025-03-08 00:40:06 +0800

    update

--- backend/index.js
+++ backend/index.js
@@ -44,10 +44,10 @@ app.use("/api/notifications", notificationRoutes);
 app.use("/api/contact", contactRoutes);
 
 if (process.env.NODE_ENV === "development") {
-  app.use(express.static(path.join(__dirname, "../frontend")));
+  app.use(express.static(path.join(dirname, "../frontend")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(path.resolve(), "frontend", "index.html"));
+    res.sendFile(path.join(dirname, "frontend", "index.html"));
   });
 }
 
